b8a9a7d4469f8723c01483be0594e90ac6c71693
Hélio Luiz Monteiro Júnior <heliojr.monteiro@yahoo.com.br>
2020-05-11 02:10:00 -0300

    Exercicio 3 - incompleto

--- exercicio.js
+++ exercicio.js
@@ -91,3 +91,66 @@ const newFilter2 = newMap2.filter(function(usuario) {
 });
 
 console.log(newFilter2);
+
+// 3 - Converta as funções nos seguintes trechos de código em Arrow Functions:
+
+// 3.1
+//const arr = [1, 2, 3, 4, 5];
+// arr.map(function(item) {
+// return item + 10;
+//});
+
+console.log('ex 3.1');
+
+const arrF = [1, 2, 3, 4, 5];
+console.log(arrF.map(item => item + 10));
+
+// 3.2
+// Dica: Utilize uma constante pra function
+//const usuario = { nome: 'Diego', idade: 23 };
+//function mostraIdade(usuario) {
+// return usuario.idade;
+//}
+//mostraIdade(usuario);
+
+console.log('ex 3.2');
+
+const usuario = { nome: 'Diego', idade: 23 };
+const usuario2 = { nome: 'Helio', idade: 31 };
+
+mostraIdade = (usuario) => {usuario.idade};
+
+// 3.3
+// Dica: Utilize uma constante pra function
+//const nome = "Diego";
+//const idade = 23;
+//function mostraUsuario(nome = 'Diego', idade = 18) {
+// return { nome, idade };
+//}
+//mostraUsuario(nome, idade);
+//mostraUsuario(nome);
+
+console.log('ex 3.3');
+
+const nome = "Diego";
+const idade = 23;
+function mostraUsuario(nome = 'Diego', idade = 18) {
+ return { nome, idade };
+}
+mostraUsuario(nome, idade);
+mostraUsuario(nome);
+
+
+// 3.4
+console.log('ex 3.4');
+//const promise = function() {
+// return new Promise(function(resolve, reject) {
+//    return resolve();
+//})
+//}
+
+const promise = function() {
+    return new Promise(function(resolve, reject) {
+        return resolve();
+    })
+}
