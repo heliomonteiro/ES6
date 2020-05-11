1292ebae29cfc12af18972a887e3617f6605521d
Hélio Luiz Monteiro Júnior <heliojr.monteiro@yahoo.com.br>
2020-05-10 21:32:43 -0300

    8 TEMPLATE LITERALS

---
+++ 8_template_literals.js
@@ -0,0 +1,7 @@
+// TEMPLATE LITERALS
+
+const nome = "Hélio";
+const idade = 31;
+
+//console.log('Meu nome é '+nome+' e tenho '+idade+' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
