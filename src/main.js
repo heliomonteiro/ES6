fba992979853f6c2ad66ffd2eb5d9dcd58ff48c4
Hélio Luiz Monteiro Júnior <heliojr.monteiro@yahoo.com.br>
2020-05-11 22:31:14 -0300

    Exercicios webpack

--- src/main.js
+++ src/main.js
@@ -13,4 +13,33 @@
 //console.log('funcoes importadas de uma vez');
 //console.log(funcoes.soma(3,2));
 
-alert("testando ok!!");
+//alert("testando ok!!");
+
+//Todos os exercícios abaixo necessitam que você esteja com o Webpack configurado.
+
+/*1º exercício
+Crie um arquivo chamado functions.js com o seguinte conteúdo:
+export const idade = 23;
+export default class Usuario {
+ static info() {
+ console.log('Apenas teste');
+ }
+}
+console.log('ex 1.1');
+Agora em seu arquivo principal import apenas a classe Usuario renomeando-a para ClasseUsuario
+e chame a funão info();
+*/
+
+import ClasseUsuario from './functions';
+ClasseUsuario.info();
+
+console.log('ex 1.2');
+//Em seu arquivo principal importe a variável de idade e exiba a mesma em tela;
+import {idade} from './functions';
+console.log(idade);
+
+
+console.log('ex 1.3');
+//Em seu arquivo principal importe tanto a classe quanto a variável idade e renomeie a variável idade
+//para IdadeUsuario.
+import ClasseUsuario2, {idade as Idadeusuario} from './functions';
